0a187e1289556b7584f56dc233ff98253f05f508
Amarendra-das <b122013@iiit-bh.ac.in>
2025-08-27 19:55:35 +0530

    .

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const path =require('path');
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
 const app = express();
 const PORT = 8000;
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 
 mongoose.connect("mongodb://localhost:27017/blogify").then((e) => console.log("mongoDB connected"))
 
@@ -12,9 +14,13 @@ app.set("view engine","ejs");
 app.set("views",path.resolve("./views"))
 
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"))
 
 app.get("/",(req,res) =>{
- res.render("home")
+ res.render("home",{
+    user: req.user,
+ });
 })
 
 app.use("/user", userRoute);
