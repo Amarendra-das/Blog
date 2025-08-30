0d8c1d4f2a11e77050a118558a29781dc4cc3337
Amarendra-das <b122013@iiit-bh.ac.in>
2025-08-30 23:45:32 +0530

    fix the route issue

--- routes/blog.js
+++ routes/blog.js
@@ -3,17 +3,14 @@ const multer = require('multer');
 const path = require('path');
 const Blog = require('../models/blog');
 const Comment = require('../models/comment');
-
-
 const cloudinary = require('cloudinary').v2;
 
 const router = Router();
 
-
-
-
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+
 
 router.get('/add-new', (req, res) => {
     return res.render('addBlog', {
@@ -21,14 +18,61 @@ router.get('/add-new', (req, res) => {
     });
 });
 
+router.get('/edit/:id', async (req, res) => {
+    const blog = await Blog.findById(req.params.id);
+    return res.render('editBlog', {
+        user: req.user,
+        blog,
+    });
+});
+
+
 router.get('/:id', async (req, res) => {
     const blog = await Blog.findById(req.params.id).populate('createdBy');
     const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy');
+    
     return res.render('blog', {
         user: req.user,
         blog,
         comments,
     });
+});
+
+
+
+router.post('/', upload.single('coverImage'), async (req, res) => {
+    const { title, body } = req.body;
+    cloudinary.uploader.upload_stream({ resource_type: 'image' }, async (error, result) => {
+        if (error) {
+            console.error('Cloudinary upload error:', error);
+            return res.status(500).send('Error uploading image');
+        }
+        const blog = await Blog.create({
+            body,
+            title,
+            createdBy: req.user._id,
+            coverImageURL: result.secure_url,
+        });
+        return res.redirect(`/blog/${blog._id}`);
+    }).end(req.file.buffer);
+});
+
+router.post('/edit/:id', upload.single('coverImage'), async (req, res) => {
+    const { title, body } = req.body;
+    const updateData = { title, body };
+
+    if (req.file) {
+        const result = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
+                if (error) reject(error);
+                resolve(result);
+            }).end(req.file.buffer);
+        });
+        updateData.coverImageURL = result.secure_url;
+    }
+
+    await Blog.findByIdAndUpdate(req.params.id, updateData);
+    return res.redirect(`/blog/${req.params.id}`);
 });
 
 router.post('/comment/:blogId', async (req, res) => {
@@ -40,68 +84,7 @@ router.post('/comment/:blogId', async (req, res) => {
     return res.redirect(`/blog/${req.params.blogId}`);
 });
 
-
-router.post('/', upload.single('coverImage'), async (req, res) => {
-    const { title, body } = req.body;
-
-    
-    if (!req.file) {
-        
-    }
-
-    
-    cloudinary.uploader.upload_stream({ resource_type: 'image' }, async (error, result) => {
-        if (error) {
-            console.error('Cloudinary upload error:', error);
-            return res.status(500).send('Error uploading image');
-        }
-
-        const blog = await Blog.create({
-            body,
-            title,
-            createdBy: req.user._id,
-            coverImageURL: result.secure_url,
-        });
-
-        return res.redirect(`/blog/${blog._id}`);
-    }).end(req.file.buffer);
-});
-
-
-router.get('/edit/:id', async (req, res) => {
-    const blog = await Blog.findById(req.params.id);
-    return res.render('editBlog', {
-        user: req.user,
-        blog,
-    });
-});
-
-// Add this missing route to routes/blog.js
-router.post('/edit/:id', upload.single('coverImage'), async (req, res) => {
-    const { title, body } = req.body;
-    const updateData = { title, body };
-
-    // Check if a new cover image was uploaded
-    if (req.file) {
-        // Upload the new image to Cloudinary
-        const result = await new Promise((resolve, reject) => {
-            cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
-                if (error) reject(error);
-                resolve(result);
-            }).end(req.file.buffer);
-        });
-        // Add the new image URL to our update data
-        updateData.coverImageURL = result.secure_url;
-    }
-
-    // Find the blog by ID and update it with the new data
-    await Blog.findByIdAndUpdate(req.params.id, updateData);
-
-    return res.redirect(`/blog/${req.params.id}`);
-});
-
 router.delete('/delete/:id', async (req, res) => {
-    
     await Blog.findByIdAndDelete(req.params.id);
     return res.redirect('/');
 });
